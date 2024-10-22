9cdfa9b92eeb6834f6096bb967234d9f65149da4
anpham9198 <anpham9198@gmail.com>
2024-10-22 19:57:56 +0700

    Add project files.

---
+++ BaiTapXayDungLopFan/Fan.cs
@@ -0,0 +1,45 @@
+﻿public class Fan
+{
+    const int SLOW = 1;
+
+    const int MEDIUM = 2;
+
+    const int HIGH = 3;
+
+    private int speed = SLOW;
+
+    private bool on = false;
+
+    private double radius = 5;
+
+    private string color = "blue";
+
+    public int Speed {  get { return speed; } set { speed = value; } }
+
+    public bool On { get { return on; } set { on = value; } }
+
+    public double Radius { get { return radius; } set { radius = value; } }
+
+    public string Color { get { return color; } set { color = value; } }
+
+    public Fan(int speed, bool on, double radius, string color)
+    {
+
+    }
+    public Fan()
+    {
+
+    }
+    override public string ToString()
+    {
+        if (on == true)
+        {
+            return "Fans is On";
+        }
+        else
+        {
+            return "Fans is Off";
+        }
+    }
+}
+
---
+++ BaiTapXayDungLopFan/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Fan fan1 = new Fan(10,true,10,"yellow");
+        Fan fan2 = new Fan(5,false,5,"Blue");
+
+        Console.WriteLine(fan1.ToString());
+        Console.WriteLine(fan2.ToString()); 
+    }
+}
